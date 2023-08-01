37d1562e677e25875e2934394954df6625358278
bficara <bficara@phmc.org>
2023-08-01 13:56:25 -0400

    -re-working font-end design -group and order options working for DB and CSV source -next is filter by value

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -17,6 +17,8 @@ export class HomepageComponent {
   uploadSuccess: boolean | null = null;
   deleteSuccess: boolean | null = null;
 
+  showSubmitToDb: boolean = false;
+
   //Filter Parameters
   makeOptions: string[] = [];
   yearOptions: string[] = [];
@@ -25,11 +27,19 @@ export class HomepageComponent {
   selectedFilterByOption: string = 'Make';
   selectedGroupByOption: string = 'Year';
 
+  priceFilter: number = 0;
+
   //Sort and Order Parameters
-  selectedSortDirOption: string = 'ASC';
+  selectedSortDirOption: string = 'asc';
       // Code for disabling filter sections
-  isSortOrderActive: boolean = true;  // 'Sort & Order' is active by default
+  isGroupSortActive: boolean = true;  // 'Sort & Order' is active by default
   isFilterActive: boolean = false;  // 'Filter' is not active by default
+
+  //choosing datasource
+
+  selectedDatasource: string = 'csv';
+
+  queryParams: any = {};
 
   constructor(private http: HttpClient,private carService: CarService) {
     this.fileToUpload = null;
@@ -41,13 +51,160 @@ export class HomepageComponent {
     this.populateYearOptions();
     }
 
+  submitRequest() {
+    if (this.isGroupSortActive) {
+
+      if (this.selectedDatasource == 'csv') {
+        switch (this.selectedGroupByOption) {
+          case 'Year':
+            this.csvToPdfByYear();
+            break;
+          case 'Make':
+            this.csvToPdfByMake()
+            break;
+          default:
+            break;
+        }
+      }
+
+      if (this.selectedDatasource == 'h2') {
+        switch (this.selectedGroupByOption) {
+          case 'Year':
+            this.carService.getAllByYearDB(this.selectedSortDirOption).subscribe(this.getObserverForPdfDownload())
+            break;
+          case 'Make':
+            this.carService.getAllByMakeDB(this.selectedSortDirOption).subscribe(this.getObserverForPdfDownload())
+            break;
+          default:
+            break;
+        }
+      }
+
+    }
+    if (this.isFilterActive) {
+      this.queryParams["filterBy"] = this.selectedFilterByOption;
+
+      switch (this.selectedFilterByOption) {
+        case 'Make':
+          this.carService.getCarsByMake(this.selectedMake ? this.selectedMake : '', this.selectedDatasource);
+          break;
+        case 'Year':
+          this.carService.getCarsByYear(Number(this.selectedYear), this.selectedDatasource);
+          break;
+        case 'Price':
+
+          this.carService.getAllCarsLessThan(this.priceFilter, this.selectedDatasource);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+
+
+//CSV TO PDF
+  csvToPdfByYear() {
+    if (this.fileToUpload) {
+      this.errorMessage = null;
+      const formData: FormData = new FormData();
+      formData.append('file', this.fileToUpload, this.fileToUpload.name);
+      this.carService.getAllByYearCSV(this.selectedSortDirOption,formData).subscribe(this.getObserverForPdfDownload())
+    } else {
+      this.errorMessage = 'No file selected';
+    }
+  }
+
+  csvToPdfByMake() {
+    if (this.fileToUpload) {
+      this.errorMessage = null;
+      const formData: FormData = new FormData();
+      formData.append('file', this.fileToUpload, this.fileToUpload.name);
+      this.carService.getAllByMakeCSV(this.selectedSortDirOption,formData).subscribe(this.getObserverForPdfDownload())
+    } else {
+      this.errorMessage = 'No file selected';
+    }
+  }
+
+  //DATABASE
+  insertCSVToDb() {
+    if (this.fileToUpload) {
+      this.errorMessage = null;
+      const formData: FormData = new FormData();
+      formData.append('file', this.fileToUpload, this.fileToUpload.name);
+      this.carService.insertCsvToDb(formData).subscribe({
+        next: (response: any) => {
+          console.log(response);
+        },
+        error: (error: any) => {
+          console.log('An error occurred:', error);
+        },
+        complete: () => {
+          console.log('Request completed');
+          this.uploadSuccess = true;
+          setTimeout(()=>{
+            this.uploadSuccess = null;
+          }, 3000)
+          this.fileToUpload = null;
+          if (this.fileInputEl) {
+            this.fileInputEl.value = '';
+          }
+
+          this.populateMakeOptions()
+          this.showSubmitToDb = false;
+        }
+      })
+    } else {
+      this.errorMessage = 'No file selected';
+      this.showSubmitToDb = false;
+    }
+  }
+
+  deleteAllFromDb() {
+    this.carService.deleteAllFromDb().subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (error: any) => {
+        console.log('An error occurred:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+        this.deleteSuccess = true;
+        setTimeout(() => {
+          this.deleteSuccess = null;
+        }, 3000);
+
+        this.populateMakeOptions()
+      }
+    });
+
+  }
+
+  getObserverForPdfDownload() {
+    return {
+      next: (response: any) => {
+        console.log(response);
+        const blob = new Blob([response.body], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+      },
+      error: (error: any) => {
+        console.log('An error occurred:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    }
+  }
+
   onSortOrderButtonClick(): void {
-    this.isSortOrderActive = true;
+    this.isGroupSortActive = true;
     this.isFilterActive = false;
   }
 
   onFilterButtonClick(): void {
-    this.isSortOrderActive = false;
+    this.isGroupSortActive = false;
     this.isFilterActive = true;
   }
 
@@ -76,117 +233,6 @@ export class HomepageComponent {
         this.errorMessage = null;
       } else {
         this.errorMessage = 'No file selected';
-      }
-    }
-  }
-  csvToPdfByYear() {
-    if (this.fileToUpload) {
-      this.errorMessage = null;
-      const formData: FormData = new FormData();
-      formData.append('file', this.fileToUpload, this.fileToUpload.name);
-      this.carService.csvToPdfByYear(formData).subscribe(this.getObserverForPdfDownload())
-    } else {
-      this.errorMessage = 'No file selected';
-    }
-  }
-
-  insertCSVToDb() {
-    if (this.fileToUpload) {
-      this.errorMessage = null;
-      const formData: FormData = new FormData();
-      formData.append('file', this.fileToUpload, this.fileToUpload.name);
-      this.carService.insertCsvToDb(formData).subscribe({
-        next: (response: any) => {
-          console.log(response);
-        },
-        error: (error: any) => {
-          console.log('An error occurred:', error);
-        },
-        complete: () => {
-          console.log('Request completed');
-          this.uploadSuccess = true;
-          setTimeout(()=>{
-            this.uploadSuccess = null;
-          }, 3000)
-          this.fileToUpload = null;
-          if (this.fileInputEl) {
-            this.fileInputEl.value = '';
-          }
-
-          this.populateMakeOptions()
-        }
-      })
-    } else {
-      this.errorMessage = 'No file selected';
-    }
-  }
-
-  csvToPdfByMake() {
-    if (this.fileToUpload) {
-      this.errorMessage = null;
-      const formData: FormData = new FormData();
-      formData.append('file', this.fileToUpload, this.fileToUpload.name);
-      this.carService.csvToPdfByMake(formData).subscribe(this.getObserverForPdfDownload())
-    } else {
-      this.errorMessage = 'No file selected';
-    }
-  }
-
-
-  deleteAllFromDb() {
-    this.carService.deleteAllFromDb().subscribe({
-      next: (response: any) => {
-        console.log(response);
-      },
-      error: (error: any) => {
-        console.log('An error occurred:', error);
-      },
-      complete: () => {
-        console.log('Request completed');
-        this.deleteSuccess = true;
-        setTimeout(() => {
-          this.deleteSuccess = null;
-        }, 3000);
-
-        this.populateMakeOptions()
-      }
-    });
-
-  }
-
-
-  getAllFromDBByYear() {
-    this.carService.getAllFromDBByYear().subscribe(this.getObserverForPdfDownload()); //adjust the URL if needed
-  }
-  getAllFromDBByMake() {
-    this.carService.getAllFromDBByMake().subscribe(this.getObserverForPdfDownload()); //adjust the URL if needed
-  }
-
-  selectByYear() {
-    let year: number | null = null;
-    if (this.yearControl.value) {
-      year = parseInt(this.yearControl.value);
-    }
-    if (year) {
-      this.carService.getCarsByYear(year).subscribe(this.getObserverForPdfDownload())
-    } else {
-      console.log(("no year select"))
-    }
-  }
-
-  getObserverForPdfDownload() {
-    return {
-      next: (response: any) => {
-        console.log(response);
-        const blob = new Blob([response.body], { type: 'application/pdf' });
-        const url = window.URL.createObjectURL(blob);
-        window.open(url);
-      },
-      error: (error: any) => {
-        console.log('An error occurred:', error);
-      },
-      complete: () => {
-        console.log('Request completed');
       }
     }
   }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -40,25 +40,59 @@ export class CarService {
     });
   }
 
-  getAllFromDBByYear(): Observable<any> {
-    const getUrl = `${this.BASE_URL}/report/h2/groupByYear`; // adjust this to your actual endpoint
+  //CSV Source
+  getAllByYearCSV( sortDir: string, formData: FormData): Observable<any> {
+    const Url = `${this.BASE_URL}/report/csv/groupByYear/${sortDir}`;
+    return this.http.post(Url, formData, {
+      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
+      observe: 'response'
+    });
+  }
+
+  getAllByMakeCSV(sortDir: string, formData: FormData): Observable<any> {
+    const getUrl = `${this.BASE_URL}/report/csv/groupByMake/${sortDir}`; // adjust this to your actual endpoint
+    return this.http.post(getUrl,formData,{
+      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
+      observe: 'response'
+    });
+  }
+
+  //DATABASE Source
+
+  getAllByMakeDB(sortDir: string): Observable<any> {
+    const getUrl = `${this.BASE_URL}/report/h2/groupByMake/${sortDir}`; // adjust this to your actual endpoint
     return this.http.get(getUrl,{
       responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
       observe: 'response'
     });
   }
 
-  getAllFromDBByMake(): Observable<any> {
-    const getUrl = `${this.BASE_URL}/report/h2/groupByMake`; // adjust this to your actual endpoint
-    return this.http.get(getUrl,{
-      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
-      observe: 'response'
-    });
-  }
-
-  getCarsByYear(year: number): Observable<any> {
-    const yearUrl = `${this.BASE_URL}/report/h2/year/${year}`;
+  getAllByYearDB(sortDir: string): Observable<any> {
+    const yearUrl = `${this.BASE_URL}/report/h2/groupByYear/${sortDir}`;
     return this.http.get(yearUrl,{
+      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
+      observe: 'response'
+    });
+  }
+
+  getCarsByYear(year: number, dataSource: string): Observable<any> {
+    const yearUrl = `${this.BASE_URL}/report/${dataSource}/year/${year}`;
+    return this.http.get(yearUrl,{
+      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
+      observe: 'response'
+    });
+  }
+
+  getCarsByMake(make: string, dataSource: string): Observable<any> {
+    const makeUrl = `${this.BASE_URL}/report/${dataSource}/make/${make}`;
+    return this.http.get(makeUrl,{
+      responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
+      observe: 'response'
+    });
+  }
+  getAllCarsLessThan(price: number, dataSource: string): Observable<any> {
+    const priceUrl = `${this.BASE_URL}/report/${dataSource}/price/${price}`;
+    return this.http.get(priceUrl,{
       responseType: 'blob' as 'json', // Expecting a blob in response (PDF File)
       observe: 'response'
     });
